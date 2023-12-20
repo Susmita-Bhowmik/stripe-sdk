30ba8c1ba78c357bf5316aef7a5376f84f128d20
susmita <susmita16032018@gmail.com>
2023-12-20 19:12:20 +0530

    first commit

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'codingbirds');
+/**
+ * Stripe API configuration
+ */
+define('STRIPE_API_SECRET_KEY', 'sk_test_tR3PYbcVNZZ796tH88S4VQ2u');
